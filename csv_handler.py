c33549a650089eee53ca7d796e335aa584c3012e
onerhook <141339052+onerhook@users.noreply.github.com>
2024-11-11 19:27:32 +0300

    Update csv_handler.py

--- csv_handler.py
+++ csv_handler.py
@@ -1,1 +1,16 @@
+import csv
 
+class CSVHandler:
+    def import_books(self, file_path):
+        books = []
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                books.append(row)
+        return books
+
+    def export_books(self, books, file_path):
+        with open(file_path, mode='w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            for book in books:
+                writer.writerow(book)
