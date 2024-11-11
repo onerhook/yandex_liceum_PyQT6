e02747a9cd2bfa529109ca95d62eb696dfe10b1f
onerhook <141339052+onerhook@users.noreply.github.com>
2024-11-11 19:27:06 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,42 @@
 import sqlite3
 
-class Database:
-    def __init__(self, db_name='tasks.db'):
-        """Инициализация подключения к базе данных."""
-        self.conn = sqlite3.connect(db_name)
-        self.cursor = self.conn.cursor()
-        self.setup_db()
+class DatabaseManager:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.create_table()
 
-    def setup_db(self):
-        """Создание таблицы задач в базе данных, если она не существует."""
-        self.cursor.execute("""
-        CREATE TABLE IF NOT EXISTS tasks (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            deadline TEXT,
-            priority INTEGER,
-            status TEXT,
-            notes TEXT
-        )
-        """)
-       
+    def create_table(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS books
+                          (id INTEGER PRIMARY KEY,
+                           title TEXT,
+                           author TEXT,
+                           year INTEGER,
+                           image TEXT)''')
+        conn.commit()
+        conn.close()
+
+    def add_book(self, title, author, year, image):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO books (title, author, year, image)
+                          VALUES (?, ?, ?, ?)''', (title, author, year, image))
+        conn.commit()
+        conn.close()
+
+    def search_books(self, title):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute('''SELECT * FROM books WHERE title LIKE ?''', ('%' + title + '%',))
+        books = cursor.fetchall()
+        conn.close()
+        return books
+
+    def get_all_books(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM books')
+        books = cursor.fetchall()
+        conn.close()
+        return books
